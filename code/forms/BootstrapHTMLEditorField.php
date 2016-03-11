46ef1ff4ecbc0cbd68f0e986124c64c570995703
andrelohmann <lohmann.andre@gmail.com>
2016-03-11 14:46:13 +0100

    first checkin of new BootstrapHTMLEditorField

---
+++ code/forms/BootstrapHTMLEditorField.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Andre Lohmann
+ * 
+ * @package geoform
+ * @subpackage fields-formattedinput
+ */
+class BootstrapHTMLEditorField extends TextareaField {
+	
+	public function Field($properties = array()) {
+		Requirements::css('bootstrap_extra_fields/thirdparty/summernote/summernote.css');
+		Requirements::javascript(FRAMEWORK_DIR . '/thirdparty/jquery/jquery.min.js');
+		Requirements::javascript('bootstrap_extra_fields/thirdparty/summernote/summernote.min.js');
+		
+		// set caption if required
+		$js = <<<JS
+$(document).ready(function() {
+    $('#{$this->ID()}').summernote({
+        callbacks: {
+            onChange: function(contents, $editable) {
+                $('textarea[name="{$this->getName()}"]').html($('#{$this->ID()}').code());
+            }
+        },
+		height: 300
+	});
+});
+JS;
+        
+		Requirements::customScript($js, 'BootstrapHTMLEditorField_Js_'.$this->ID());
+        
+        return parent::Field($properties);
+	}
+}
