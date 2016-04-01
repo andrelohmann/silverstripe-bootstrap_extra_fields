c50be42211ac54e62ff3f613b8341e1fb38115cb
andrelohmann <lohmann.andre@gmail.com>
2016-04-01 20:29:12 +0200

    added bootstrap liquid field

---
+++ code/forms/BootstrapLiquidEditorField.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Andre Lohmann
+ * 
+ * @package bootstrap-extra-fields
+ */
+class BootstrapLiquidEditorField extends TextareaField {
+	
+	public function Field($properties = array()) {
+		Requirements::javascript('bootstrap_extra_fields/thirdparty/ace-min-noconflict/ace.js');
+		
+		// set caption if required
+		$js = <<<JS
+$(document).ready(function() {
+	var editor = ace.edit("{$this->ID()}");
+	editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/liquid");
+    editor.getSession().on('change', function(e) {
+		$('#{$this->ID()}').html(editor.getValue());
+	});
+});
+JS;
+        
+		Requirements::customScript($js, 'BootstrapLiquidEditorField_Js_'.$this->ID());
+        
+        return parent::Field($properties);
+	}
+}
