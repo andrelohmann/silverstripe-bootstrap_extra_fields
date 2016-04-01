57c7ef02427eb0c2012f38f7d7d5fbb0511d07f9
andrelohmann <lohmann.andre@gmail.com>
2016-04-01 21:40:51 +0200

    fixed BootstrapLiquidEditorField

--- code/forms/BootstrapLiquidEditorField.php
+++ code/forms/BootstrapLiquidEditorField.php
@@ -7,12 +7,19 @@
 class BootstrapLiquidEditorField extends TextareaField {
 	
 	public function Field($properties = array()) {
+		$css = <<<CSS
+#{$this->ID()}-ace-editor {
+    height: 300px;
+}
+CSS;
+		Requirements::customCSS($css, 'BootstrapLiquidEditorField_Css_'.$this->ID());
 		Requirements::javascript('bootstrap_extra_fields/thirdparty/ace-min-noconflict/ace.js');
 		
 		// set caption if required
 		$js = <<<JS
 $(document).ready(function() {
 	var editor = ace.edit("{$this->ID()}-ace-editor");
+	editor.setValue($('#{$this->ID()}').val());
 	editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/liquid");
     editor.getSession().on('change', function(e) {
